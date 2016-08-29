c90bc7e0f9a595f493df21b5d16fa5cd36600729
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-08-29 18:39:32 +0200

    Add base interface

---
+++ app/laserweb/laserweb.js
@@ -0,0 +1,18 @@
+/**
+* LaserWeb Global Namespace.
+*
+* All stuff related to LaserWeb must be set in this scope.
+*/
+var laserweb = {
+
+    // LaserWeb version
+    version: '4.0.0',
+
+    // Modules collection
+    modules: {},
+
+    // Initialisation
+    init: function() {
+
+    }
+};
