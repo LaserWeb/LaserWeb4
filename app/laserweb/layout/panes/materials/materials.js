b0b2b9c69f63ff83cf976857f440ad036e9dc0fe
Richard Betel <emteeoh@gmail.com>
2016-08-31 12:29:43 -0400

    added a materials tab.

---
+++ app/laserweb/layout/panes/materials/materials.js
@@ -0,0 +1,27 @@
+;(function(lw) {
+
+    /**
+    * LaserWeb materials module.
+    *
+    * Description...
+    */
+    lw.add_module('layout.panes.materials', {
+
+        // Autoload the module ?
+        autoload: true,
+
+        // Module version
+        version: '0.0.1',
+
+        // Module title
+        title: 'Materials',
+
+        // Module icon
+        icon: 'bookmark',
+
+        // Extends
+        extends: ['layout.pane']
+
+    });
+
+})(laserweb);
