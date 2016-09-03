3e0818e30bd26f4e0b9a2421d7c5684ac541a29f
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-09-03 13:45:01 +0200

    Socket.io connection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 var config = require('./config');
 var http   = require('./servers/http');
+var serial = require('./servers/serial');
 var app    = require('http').createServer(http.handler);
-var io     = require('socket.io')(app, { path: '/vendor/socket.io' });
+var io     = require('socket.io');
 var os     = require('os');
 
 app.listen(config.port, os.hostname(), function() {
     var host = app.address();
+
     console.log(http.footprint);
     console.log('Listening on http://%s:%s', host.address, host.port);
-});
 
-io.on('connection', function(socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
+    var ws = io(app, { path: '/vendor/socket.io' });
+    
+    ws.on('connection', serial.attach);
 });
