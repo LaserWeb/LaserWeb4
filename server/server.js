ba85b03638b45913adb1724395ac4bb82005e3a9
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-09-03 08:09:24 +0200

    Get and print host ip

--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@ var config = require('./config');
 var http   = require('./servers/http');
 var app    = require('http').createServer(http.handler);
 var io     = require('socket.io')(app, { path: '/vendor/socket.io' });
+var os     = require('os');
 
-app.listen(config.port, function() {
+app.listen(config.port, os.hostname(), function() {
+    var host = app.address();
     console.log(http.footprint);
-    console.log('Listening on http://localhost:%s', config.port);
+    console.log('Listening on http://%s:%s', host.address, host.port);
+});
+
+io.on('connection', function(socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
