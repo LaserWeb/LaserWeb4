700824c02edad5ebcec51b241233d3f78647c7cb
Todd Fleming <tbfleming@gmail.com>
2016-10-09 01:36:18 -0400

    THREE

--- src/components/workspace.js
+++ src/components/workspace.js
@@ -4,9 +4,9 @@
  * @module
  */
 
-// React/Redux
 import React from 'react'
 import { connect } from 'react-redux'
+import * as THREE from 'three';
 
 /**
  * Workspace component.
@@ -16,14 +16,55 @@ import { connect } from 'react-redux'
  * @param {Object} props Component properties.
  */
 class Workspace extends React.Component {
-    /**
-     * Render the component.
-     * @return {String}
-     */
+    setCanvas(canvas) {
+        if (canvas === this.canvas)
+            return;
+        this.canvas = canvas;
+        if (!this.canvas)
+            return;
+
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.z = 1000;
+        let geometry = new THREE.BoxGeometry(200, 200, 200);
+        let material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+        this.renderer = new THREE.WebGLRenderer({ canvas });
+
+        let animate = () => {
+            if (!this.canvas)
+                return;
+            let w = Math.floor(this.canvas.clientWidth * window.devicePixelRatio);
+            let h = Math.floor(this.canvas.clientHeight * window.devicePixelRatio);
+            if (this.canvas.width != w || this.canvas.height != h) {
+                this.canvas.width = w;
+                this.canvas.height = h;
+                this.needRedraw = true;
+            }
+            if (this.needRedraw) {
+                this.mesh.rotation.x += 0.1;
+                this.mesh.rotation.y += 0.2;
+                this.renderer.render(this.scene, this.camera);
+                this.needRedraw = false;
+            }
+            requestAnimationFrame(animate);
+        };
+        animate();
+    }
+
     render() {
+        this.needRedraw = true;
         return (
-            <div id="workspace" className={ (this.props.fullWidth ? "full-width" : "") + " full-height" }>
-                <p>workspace</p>
+            <div id="workspace" className="full-height">
+                <div className="canvas-div">
+                    <canvas ref={canvas => this.setCanvas(canvas)} />
+                </div>
+                <div className="workspace-controls">
+                    <p />
+                    <b>Stuff goes here...</b>
+                    <p />
+                </div>
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['react'],
