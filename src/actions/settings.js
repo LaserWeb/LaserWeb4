a0059edb36424c36a1d927065319e580380b9693
Jorge Robles <jorge.diego.robles@gmail.com>
2016-10-28 14:12:47 +0200

    Basic Settings UI

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -1,3 +1,6 @@
 import { setAttrs } from '../actions/object'
 
 export const setSettingsAttrs = setAttrs('settings');
+
+export const uploadSettings = (file, content) => ({ /*SETTINGS_UPLOAD*/ type:"SETTINGS_SET_ATTRS", payload: {attrs:JSON.parse(content)}});
+export const downloadSettings = (settings) => ({ type:"SETTINGS_DOWNLOAD", payload: settings});
