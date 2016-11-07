9af81e4bba6532073bfb9f256da2d72490d6233e
jorgerobles <jorge.diego.robles@gmail.com>
2016-11-07 13:43:10 +0100

    Settings ui (#26)
    Further validation.
    Localstorage saving

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { applyMiddleware, createStore } from 'redux';
+import { compose, applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
-const middleware = applyMiddleware(logger({ collapsed: true }));
+import persistState, {mergePersistedState} from 'redux-localstorage'
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+import filter from 'redux-localstorage-filter';
+
 
 const hot = (state, action) => {
     return require('./reducers').default(state, action);
 };
 
-const store = createStore(hot, middleware);
+const reducer = compose(
+  mergePersistedState()
+)(hot);
+
+const storage = compose(
+  filter('settings')
+)(adapter(window.localStorage));
+
+const middleware = compose(
+  applyMiddleware( logger({ collapsed: true })),
+  persistState(storage, 'LaserWeb')
+);
+
+const store = createStore(reducer, middleware);
 
 function Hot(props) {
     const LaserWeb = require('./components/laserweb').default;
