341a9cd1dda0ac8b44d9463e0ba594636e7ce8c3
Todd Fleming <tbfleming@gmail.com>
2016-11-20 11:32:32 -0500

    Make gcode editable

--- src/components/gcode.js
+++ src/components/gcode.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { setGcode } from '../actions/gcode';
+
 class Gcode extends React.Component {
     render() {
         return (
             <div style={{ overflow: 'hidden', height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <textarea style={{ flexGrow: 2 }} value={this.props.gcode} />
+                <textarea style={{ flexGrow: 2 }} value={this.props.gcode} onChange={this.props.setGcode} />
             </div>
         )
     }
 };
 Gcode = connect(
-    state => ({ gcode: state.gcode })
+    state => ({ gcode: state.gcode }),
+    dispatch => ({ setGcode: e => dispatch(setGcode(e.target.value)) })
 )(Gcode);
 
 export default Gcode;
