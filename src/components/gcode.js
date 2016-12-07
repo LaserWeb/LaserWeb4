561f0c3b676cf9684ad41a3abe60dec1a03b1f99
Todd Fleming <tbfleming@gmail.com>
2016-12-07 09:43:13 -0500

    Save gcode

--- src/components/gcode.js
+++ src/components/gcode.js
@@ -2,18 +2,27 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { setGcode } from '../actions/gcode';
+import { sendAsFile } from '../lib/helpers';
 
 class Gcode extends React.Component {
     render() {
+        let textArea;
+        // textArea = <textarea style={{ flexGrow: 2 }} value={this.props.gcode} onChange={this.props.setGcode} />;
         return (
             <div style={{ overflow: 'hidden', height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <textarea style={{ flexGrow: 2 }} value={this.props.gcode} onChange={this.props.setGcode} />
+                <div>
+                    <button onClick={this.props.saveGcode}>Save GCode</button>
+                </div>
+                {textArea}
             </div>
         )
     }
 };
 Gcode = connect(
-    state => ({ gcode: state.gcode }),
+    state => ({
+        gcode: state.gcode,
+        saveGcode: () => sendAsFile('gcode.gcode', state.gcode),
+    }),
     dispatch => ({ setGcode: e => dispatch(setGcode(e.target.value)) })
 )(Gcode);
 
