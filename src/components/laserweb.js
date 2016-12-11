23b852574859bdd6febe73b23fc1fc0962d4aa93
openhardwarecoza <peter.plaaswerf@gmail.com>
2016-12-11 11:52:42 +0200

    CSS fixes for sidebar menu being a little too large for low res screens

--- src/components/laserweb.js
+++ src/components/laserweb.js
@@ -72,7 +72,7 @@ class LaserWeb extends React.Component {
                 <DocumentCacheHolder style={{ width: '100%' }} documents={this.props.documents}>
                     <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
                         <Sidebar ref="sidebar" style={{ flexGrow: 0, flexShrink: 0 }}>
-                            <Com id="com" title="Communication" icon="plug" />
+                            <Com id="com" title="Comms" icon="plug" />
                             <Jog id="jog" title="Jog" icon="arrows-alt" />
                             <Cam id="cam" title="CAM" icon="pencil-square-o" />
                             <Gcode id="gcode" title="G-Code" icon="file-code-o" />
