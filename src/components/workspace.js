ea04f1d68b37c9c148ddfda534d9066a4e7d7287
uMinded <iceblu3710@gmail.com>
2017-01-02 21:56:51 -0600

    Fix a borked rebase

--- src/components/workspace.js
+++ src/components/workspace.js
@@ -158,13 +158,8 @@ class FloatingControls extends React.Component {
             vec4.transformMat4([],
                 vec4.transformMat4([], [bounds.x1, bounds.y1, 0, 1], this.props.camera.view),
                 this.props.camera.perspective);
-<<<<<<< HEAD
-            let x = ((p[0] / p[3] + 1) * this.props.workspaceWidth / 2) - 20;
-            let y = (this.props.workspaceHeight - (p[1] / p[3] + 1) * this.props.workspaceHeight / 2) + 20;
-=======
         let x = (p[0] / p[3] + 1) * this.props.workspaceWidth / 2 - 20;
         let y = this.props.workspaceHeight - (p[1] / p[3] + 1) * this.props.workspaceHeight / 2 + 20;
->>>>>>> d0f2e392d67e3ddbcc995daf8ee540fad00f0421
 
         x = x / window.devicePixelRatio - this.props.width;
         y = y / window.devicePixelRatio;
