a16d941745eb7c13333c8d49473a722a53300a56
Todd Fleming <tbfleming@gmail.com>
2017-01-10 11:12:12 -0500

    Simplify controls examples

--- src/components/com.js
+++ src/components/com.js
@@ -6,27 +6,28 @@ import { setSettingsAttrs } from '../actions/settings';
 
 class Com extends React.Component {
     render() {
+        let {settings, dispatch} = this.props;
         return (
             <div>
                 <h4>Fancy controls examples</h4>
-                <NumberField {...{ object: this.props.settings, field: 'machineWidth', setAttrs: setSettingsAttrs, description: 'Machine Width', units: 'mm' }} />
-                <ToggleField {... { object: this.props.settings, field: 'toolSafetyLockDisabled', setAttrs: setSettingsAttrs, description: 'Disable Safety Lock' }} />
+                <NumberField {...{ object: settings, field: 'machineWidth', setAttrs: setSettingsAttrs, description: 'Machine Width', units: 'mm' }} />
+                <ToggleField {... { object: settings, field: 'toolSafetyLockDisabled', setAttrs: setSettingsAttrs, description: 'Disable Safety Lock' }} />
 
                 <h4>Basic controls examples</h4>
                 Use "Input" for text and number fields. Input uses onChangeValue.
                 <br />
                 <Input
                     type="number" step="any" style={{ width: 100 }}
-                    value={this.props.settings.machineWidth}
-                    onChangeValue={value => this.props.dispatch(setSettingsAttrs({ machineWidth: value }))}
+                    value={settings.machineWidth}
+                    onChangeValue={value => dispatch(setSettingsAttrs({ machineWidth: value }))}
                     />
                 <br />
                 Use "input" for checkboxes, radio buttons, etc. input uses onChange.
                 <br />
                 CNC Mode:
                 <input type="checkbox"
-                    checked={this.props.settings.toolCncMode}
-                    onChange={e => this.props.dispatch(setSettingsAttrs({ toolCncMode: e.target.checked }))}
+                    checked={settings.toolCncMode}
+                    onChange={e => dispatch(setSettingsAttrs({ toolCncMode: e.target.checked }))}
                     />
             </div>
         )
