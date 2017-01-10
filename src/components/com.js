384988dfb58b0376f4a62a022ad6501c3476b271
Todd Fleming <tbfleming@gmail.com>
2017-01-10 10:37:24 -0500

    Control examples

--- src/components/com.js
+++ src/components/com.js
@@ -1,35 +1,40 @@
-/**
- * Communication module.
- * @module
- */
-
-// React
 import React from 'react'
+import { connect } from 'react-redux';
 
-// Main components
-import Serial from './com/serial'
-import Network from './com/network'
+import { Input, NumberField, ToggleField } from './forms';
+import { setSettingsAttrs } from '../actions/settings';
 
-/**
- * Communication component.
- *
- * @extends module:react~React~Component
- * @param {Object} props Component properties.
- */
 class Com extends React.Component {
-    /**
-     * Render the component.
-     * @return {String}
-     */
     render() {
         return (
             <div>
-                <Serial />
-                <Network />
+                <h4>Fancy controls examples</h4>
+                <NumberField {...{ object: this.props.settings, field: 'machineWidth', setAttrs: setSettingsAttrs, description: 'Machine Width', units: 'mm' }} />
+                <ToggleField {... { object: this.props.settings, field: 'toolSafetyLockDisabled', setAttrs: setSettingsAttrs, description: 'Disable Safety Lock' }} />
+
+                <h4>Basic controls examples</h4>
+                Use "Input" for text and number fields. Input uses onChangeValue.
+                <br />
+                <Input
+                    type="number" step="any" style={{ width: 100 }}
+                    value={this.props.settings.machineWidth}
+                    onChangeValue={value => this.props.dispatch(setSettingsAttrs({ machineWidth: value }))}
+                    />
+                <br />
+                Use "input" for checkboxes, radio buttons, etc. input uses onChange.
+                <br />
+                CNC Mode:
+                <input type="checkbox"
+                    checked={this.props.settings.toolCncMode}
+                    onChange={e => this.props.dispatch(setSettingsAttrs({ toolCncMode: e.target.checked }))}
+                    />
             </div>
         )
     }
 }
 
-// Exports
+Com = connect(
+    state => ({ settings: state.settings })
+)(Com);
+
 export default Com
