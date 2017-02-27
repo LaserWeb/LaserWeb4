c9959b2495ff5b192ace1009c23f553f90115a24
Jorge Robles <jorge.diego.robles@gmail.com>
2017-02-27 19:41:11 +0100

    Rasterizer on a Worker.

--- src/lib/workers/cam-raster.js
+++ src/lib/workers/cam-raster.js
@@ -1,31 +1,33 @@
 import { RasterToGcode } from 'lw.raster-to-gcode'
 
 // On messsage received
-self.onmessage = function(event) {
+self.onmessage = function (event) {
     if (event.data.cmd === 'start') {
         start(event.data);
     }
 }
 
-// Create RasterToGcode object
-var rasterToGcode = new RasterToGcode();
 
-// Register events callbacks
-rasterToGcode.on('progress', function(event) {
-    self.postMessage({ event: 'onProgress', ...event });
-})
-.on('done', function(event) {
-    self.postMessage({ event: 'onDone', ...event });
-})
-.on('abort', function() {
-    self.postMessage({ event: 'onAbort' });
-});
 
 // Start job
 function start(data) {
-    Object.assign(rasterToGcode, data.properties);
-    self.postMessage({ event: 'onStart' });
-    rasterToGcode.run(data.settings);
+
+    // Create RasterToGcode object
+    var rasterToGcode = new RasterToGcode(data.settings);
+        Object.assign( rasterToGcode, data.properties );
+    // Register events callbacks
+        rasterToGcode.on('progress', function (event) {
+            self.postMessage({ event: 'onProgress', ...event });
+        }).on('done', function (event) {
+            self.postMessage({ event: 'onDone', ...event });
+        }).on('abort', function () {
+            self.postMessage({ event: 'onAbort' });
+        });
+
+    
+    self.postMessage({ event: 'start' });
+    rasterToGcode.run();
+
 }
 
 // Abort job
