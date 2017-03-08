4aeefb6b90be849df8f24e90ad5c4efb061858ca
Jorge Robles <jorge.diego.robles@gmail.com>
2017-03-08 11:18:18 +0100

    https://github.com/LaserWeb/LaserWeb4/issues/196 Whole cam gcode queue refactor Preflight now is a promise included in the job. Raster handled by sub queue.

--- src/lib/cam-gcode-raster.js
+++ src/lib/cam-gcode-raster.js
@@ -1,11 +1,12 @@
 import RasterToGcode from 'lw.raster-to-gcode';
+import queue from 'queue'
 
-export function getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages, showAlert, done, progress, QE, workers) {
+export function getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages, showAlert, done, progress, jobIndex, QE_chunk, workers) {
 
     let ok = true;
 
     if (!(op.laserDiameter > 0)) {
-        showAlert("LaserDiameter should be greater than 0","danger");
+        showAlert("LaserDiameter should be greater than 0", "danger");
         ok = false;
     }
 
@@ -14,51 +15,62 @@ export function getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages,
         ok = false;
     }
 
-    if (!ok)
+    if (!ok) {
         done(false);
+        return [];
+    }
+
+    let gcode=[];
+
+    let QE = new queue();
+        QE.concurrency = 1;
+        QE.timeout = 3600 * 1000
+        QE.chunk = 100 / docsWithImages.length
+
+    // POSTPROCESS GCODE;
+    const postProcessing = (gc) => {
+        let g = '';
+        let raster = gc.join('\r\n') + "\r\n\r\n";
+
+        if (op.useBlower) {
+            if (settings.machineBlowerGcodeOn) {
+                g += `\r\n` + settings.machineBlowerGcodeOn + '; Enable Air assist\r\n';
+            }
+        }
 
 
-    const percentProcessing = (ev) => {
-        let jobIndex = QE.total - QE.length
-        let p = parseInt((jobIndex * QE.chunk) + (ev.percent * QE.chunk / 100))
+        for (let pass = 0; pass < op.passes; ++pass) {
+            g += '\n\n; Pass ' + pass + '\r\n';
+            if (settings.machineZEnabled) {
+                let zHeight = op.startHeight + settings.machineZToolOffset - (op.passDepth * pass);
+                g += `\r\n; Pass Z Height ${zHeight}mm (Offset: ${settings.machineZToolOffset}mm)\r\n`;
+                g += 'G0 Z' + zHeight.toFixed(settings.decimal || 3) + '\r\n';
+            }
+            g += raster;
+        }
+
+
+        if (op.useBlower) {
+            if (settings.machineBlowerGcodeOff) {
+                g += `\r\n` + settings.machineBlowerGcodeOff + '; Disable Air assist\r\n';
+            }
+        }
+
+        return g;
+    }
+
+    // FRAGMENT PROGRESS
+    const percentProcessing = (percent) => {
+        let p = parseInt((jobIndex * QE_chunk) + (percent * (QE_chunk / 100)))
         progress(p);
     }
+
 
     for (let index = 0; index < docsWithImages.length; index++) {
 
         QE.push((cb) => {
 
             const doc = docsWithImages[index]
-            const doneProcessing = (ev) => {
-                let g = '';
-                let raster = ev.gcode.join('\r\n') + "\r\n\r\n";
-
-                if (op.useBlower) {
-                    if (settings.machineBlowerGcodeOn) {
-                        g += `\r\n` + settings.machineBlowerGcodeOn + '; Enable Air assist\r\n';
-                    }
-                }
-
-                
-                for (let pass = 0; pass < op.passes; ++pass) {
-                    g += '\n\n; Pass ' + pass + '\r\n';
-                    if (settings.machineZEnabled) {
-                        let zHeight = op.startHeight + settings.machineZToolOffset - (op.passDepth * pass);
-                        g += `\r\n; Pass Z Height ${zHeight}mm (Offset: ${settings.machineZToolOffset}mm)\r\n`;
-                        g += 'G0 Z' + zHeight.toFixed(settings.decimal || 3) + '\r\n';
-                    }
-                    g += raster;
-                }
-                
-
-                if (op.useBlower) {
-                    if (settings.machineBlowerGcodeOff) {
-                        g += `\r\n` + settings.machineBlowerGcodeOff + '; Disable Air assist\r\n';
-                    }
-                }
-
-                done(g, cb)
-            }
 
             let params = {
                 ppi: { x: doc.dpi / doc.scale[0], y: doc.dpi / doc.scale[1] },
@@ -73,7 +85,7 @@ export function getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages,
                 verboseG: op.verboseGcode,
                 diagonal: op.diagonal,
                 nonBlocking: false,
-                milling:false,
+                milling: false,
                 filters: {
                     smoothing: op.smoothing,
                     brightness: op.brightness,
@@ -98,10 +110,9 @@ export function getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages,
                 let r2g = new rasterWorker();
                 r2g.onmessage = function (event) {
                     if (event.data.event === 'onDone') {
-                        doneProcessing({ gcode: event.data.gcode })
-                    }
-                    else if (event.data.event === 'onProgress') {
-                        percentProcessing({ percent: event.data.percent }) //doc, index, 
+                        gcode.push(postProcessing(event.data.gcode)); cb();
+                    } else if (event.data.event === 'onProgress') {
+                        percentProcessing((index * QE.chunk) + (event.data.percent * QE.chunk / 100))
                     }
                 };
 
@@ -113,6 +124,10 @@ export function getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages,
         })
 
     }
+
+    QE.start((err)=>{
+        done(gcode.join("\r\n"));
+    })
 
 
 }
--- src/lib/cam-gcode.js
+++ src/lib/cam-gcode.js
@@ -24,30 +24,29 @@ export function getGcode(settings, documents, operations, documentCacheHolder, s
     "use strict";
 
     const QE = new queue();
-          QE.timeout = 3600 * 1000;
-          QE.concurrency = 1;
+    QE.timeout = 3600 * 1000;
+    QE.concurrency = 1;
 
     const gcode = [];
     const workers = [];
-    let jobIndex=0;
+    let jobIndex = 0;
 
     for (let opIndex = 0; opIndex < operations.length; ++opIndex) {
         let op = operations[opIndex];
 
-        let geometry = [];
-        let openGeometry = [];
-        let tabGeometry = [];
-        let docsWithImages = [];
 
-        const jobDone = (g, cb) => { if (g !== false) { gcode.push(g); cb(); } }
 
-        const invokeWebWorker = (ww, props, cb, jobIndex) => {
-            console.log("starting job "+jobIndex)
+        const jobDone = (g, cb) => { 
+            if (g !== false) { gcode.push(g); cb(); } 
+        }
+
+        let invokeWebWorker = (ww, props, cb, jobIndex) => {
+            //console.log("starting job " + jobIndex)
             let peasant = new ww();
             peasant.onmessage = (e) => {
                 let data = JSON.parse(e.data)
                 if (data.event == 'onDone') {
-                    console.log("job done "+jobIndex)
+                    //console.log("job done " + jobIndex)
                     jobDone(data.gcode, cb)
                 } else if (data.event == 'onProgress') {
                     let p = parseInt((jobIndex * QE.chunk) + (data.progress * QE.chunk / 100))
@@ -60,67 +59,79 @@ export function getGcode(settings, documents, operations, documentCacheHolder, s
                 }
             }
             workers.push(peasant)
+            progress(jobIndex * QE.chunk);
             peasant.postMessage(props)
 
         }
 
-        QE.push((cb) => {
-            let preflightWorker = require('worker-loader!./workers/cam-preflight.js');
-            let preflight = new preflightWorker()
-            preflight.onmessage = (e) => {
-                let data = e.data;
-                if (data.event == 'onDone') {
-                    geometry = data.geometry
-                    openGeometry = data.openGeometry
-                    tabGeometry = data.tabGeometry
-                    data.docsWithImages.forEach(_doc => {
-                        let cache = documentCacheHolder.cache.get(_doc.id);
-                        if (cache && cache.imageLoaded)
-                            docsWithImages.push(Object.assign([], _doc, { image: cache.image }));
-                    })
-                    cb();
-                } else if (data.event == 'onProgress') {
-                    progress((data.percent / 100) * QE.chunk )
-                } else if (data.event == 'onError'){
-                    showAlert(data.message,data.level)
-                    QE.end()
-                }
-                
-            }
-            workers.push(preflight)
-            console.log("Preflight")
-            preflight.postMessage({ settings, documents, opIndex, op, geometry, openGeometry, tabGeometry })
+        let preflightPromise = (settings, documents, opIndex, op, workers) => {
+            return new Promise((resolve, reject) => {
+                let geometry = [];
+                let openGeometry = [];
+                let tabGeometry = [];
+                let docsWithImages = [];
 
+                let preflightWorker = require('worker-loader!./workers/cam-preflight.js');
+                let preflight = new preflightWorker()
+                preflight.onmessage = (e) => {
+                    let data = e.data;
+                    if (data.event == 'onDone') {
+                        if (data.geometry) geometry = data.geometry
+                        if (data.openGeometry) openGeometry = data.openGeometry
+                        if (data.tabGeometry) tabGeometry = data.tabGeometry
+                        data.docsWithImages.forEach(_doc => {
+                            let cache = documentCacheHolder.cache.get(_doc.id);
+                            if (cache && cache.imageLoaded)
+                                docsWithImages.push(Object.assign([], _doc, { image: cache.image }));
+                        })
+                        resolve({ geometry, openGeometry, tabGeometry, docsWithImages })
+                    } else if (data.event == 'onProgress') {
+                        progress((data.percent / 100) * QE.chunk)
+                    } else if (data.event == 'onError') {
+                        reject(data)
+                    }
+
+                }
+                workers.push(preflight)
+                preflight.postMessage({ settings, documents, opIndex, op, geometry, openGeometry, tabGeometry })
+            })
+        }
+
+
+        QE.push((cb) => {
+            console.log(op.type + "->" + jobIndex)
+            preflightPromise(settings, documents, opIndex, op, workers)
+                .then((preflight) => {
+                    let { geometry, openGeometry, tabGeometry, docsWithImages } = preflight;
+
+                    if (op.type === 'Laser Cut' || op.type === 'Laser Cut Inside' || op.type === 'Laser Cut Outside' || op.type === 'Laser Fill Path') {
+
+                        invokeWebWorker(require('worker-loader!./workers/cam-lasercut.js'), { settings, opIndex, op, geometry, openGeometry, tabGeometry }, cb, jobIndex)
+
+                    } else if (op.type === 'Laser Raster') {
+
+                        getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages, showAlert, (gcode)=>{jobDone(gcode,cb)}, progress, jobIndex, QE.chunk, workers);
+
+                    } else if (op.type.substring(0, 5) === 'Mill ') {
+
+                        invokeWebWorker(require('worker-loader!./workers/cam-mill.js'), { settings, opIndex, op, geometry, openGeometry, tabGeometry }, cb, jobIndex)
+
+                    } else {
+                        showAlert("Unknown operation " + op.type, 'warning')
+                        cb()
+                    }
+                })
+                .catch((err) => {
+                    showAlert(err.message, err.level)
+                    QE.end()
+                })
         })
 
-        if (op.type === 'Laser Cut' || op.type === 'Laser Cut Inside' || op.type === 'Laser Cut Outside' || op.type === 'Laser Fill Path') {
-
-            QE.push((cb) => {
-                console.log(op.type)
-                invokeWebWorker(require('worker-loader!./workers/cam-lasercut.js'), { settings, opIndex, op, geometry, openGeometry, tabGeometry }, cb, jobIndex)
-            })
-
-        } else if (op.type === 'Laser Raster') {
-
-            QE.push((cb) => {
-                console.log(op.type)
-                getLaserRasterGcodeFromOp(settings, opIndex, op, docsWithImages, showAlert, jobDone, progress, QE, workers);
-                cb();
-            })
-
-        } else if (op.type.substring(0, 5) === 'Mill ') {
-            console.log(op.type)
-            QE.push((cb) => {
-                invokeWebWorker(require('worker-loader!./workers/cam-mill.js'), { settings, opIndex, op, geometry, openGeometry, tabGeometry }, cb, jobIndex)
-            })
-
-        }
     } // opIndex
 
     QE.total = QE.length
     QE.chunk = 100 / QE.total
 
-    
     progress(0)
     QE.on('success', (result, job) => {
         jobIndex++
@@ -133,9 +144,9 @@ export function getGcode(settings, documents, operations, documentCacheHolder, s
                 ww.abort()
             } else {
                 ww.terminate();
-            } 
+            }
         })
-        
+
     })
 
     QE.start((err) => {
