b10ead21bde5d5c6e080a9e1f212322cec992d02
Jorge Robles <jorge.diego.robles@gmail.com>
2017-06-14 13:51:37 +0200

    Potrace working WIP

--- src/components/image-filters.js
+++ src/components/image-filters.js
@@ -218,15 +218,56 @@ export class ImageEditorButton extends React.Component {
     }
 }
 
+
+function checkRange(min, max) {
+    return {
+        check: (v) => {
+            if (isFinite(v)) {
+                return v >= min && v <= max;
+            } else if (isObject(v) && v.hasOwnProperty('min') && v.hasOwnProperty('max')) {
+                return (v.min >= min && v.min <= max) && (v.max >= min && v.max <= max)
+            }
+        },
+        error: 'Must be in range [' + min + ' , ' + max + ']',
+    }
+}
+
 function EnumInput(opts, def) {
     if (Array.isArray(opts))
         opts = Object.assign( ...opts.map(i=>({[i]:i})) )
     
     return function({ op, field, onChangeValue, ...rest }){
-        return <select value={op[field]}  {...rest} >
+        return <select value={op[field.name]}  onChange={e => onChangeValue(e.target.value)}>
             {Object.entries(opts).map((e, i)=>(<option key={i} value={e[0]}>{e[1]}</option>))}
         </select>
     }
+}
+
+function ToggleInput({ op, field, onChangeValue, fillColors, strokeColors, className = "scale75", ...rest }) {
+    return <Toggle id={"toggle_" + field.name} defaultChecked={op[field.name]} onChange={e => onChangeValue(e.target.checked)} className={className} />
+}
+
+function NumberInput(props) {
+    let { op, field, fillColors, strokeColors, ...rest } = props;
+    return <Input type='number' step='any' value={op[field.name]}   {...rest } />;
+}
+
+const ImageFilterControls={
+    smoothing:      { name: 'smoothing', label: 'Smoothing',  input: ToggleInput },                                // lw.raster-to-gcode: Smoothing the input image ?
+    brightness:     { name: 'brightness',label: 'Brightness',  input: NumberInput, ...checkRange(-255, 255) },   // lw.raster-to-gcode: Image brightness [-255 to +255]
+    contrast:       { name: 'contrast', label: 'Contrast',  input: NumberInput, ...checkRange(-255, 255) },         // lw.raster-to-gcode: Image contrast [-255 to +255]
+    gamma:          { name: 'gamma',label: 'Gamma', units: '', input: NumberInput, ...checkRange(0, 7.99) },                    // lw.raster-to-gcode: Image gamma correction [0.01 to 7.99]
+    grayscale:      { name: 'grayscale',label: 'Grayscale', units: '', input: EnumInput(['none', 'average', 'luma', 'luma-601', 'luma-709', 'luma-240', 'desaturation', 'decomposition-min', 'decomposition-max', 'red-chanel', 'green-chanel', 'blue-chanel']) },                             // lw.raster-to-gcode: Graysale algorithm [none, average, luma, luma-601, luma-709, luma-240, desaturation, decomposition-[min|max], [red|green|blue]-chanel]
+    shadesOfGray:   { name: 'shadesOfGray', label: 'Shades', units: '', input: NumberInput, ...checkRange(2, 256) },      // lw.raster-to-gcode: Number of shades of gray [2-256]
+    invertColor:    { name: 'invertColor', label: 'Invert Color', units: '', input: ToggleInput },                         // lw.raster-to-gcode
+}
+
+const ImageTraceControls={
+    turnpolicy:     { name: 'turnpolicy', label: 'Turn Policy',  input: EnumInput(['minority', 'majority','black','white','right', 'left', 'random']) },                                // potrace
+    turdsize:       { name: 'turdsize', label: 'Turd Size',  input: NumberInput,  },                                // potrace
+    alphamax:       { name: 'alphamax',  label: 'Alpha Max',  input: NumberInput,  },                                // potrace
+    optcurve:       { name: 'optcurve', label: 'Opt Curve', units: '', input: ToggleInput, },                       // potrace
+    opttolerance:   { name: 'opttolerance', label: 'Opt Tolerance', units: '', input: NumberInput },                // potrace
 }
 
 class ImageEditor extends React.Component
@@ -240,9 +281,19 @@ class ImageEditor extends React.Component
                 optcurve: true,         // potrace
                 alphamax: 1,            // potrace
                 opttolerance: 0.2,      // potrace
+            }, 
+            filters: {
+                smoothing: false,       // lw.raster-to-gcode: Smoothing the input image ?
+                brightness: 0,          // lw.raster-to-gcode: Image brightness [-255 to +255]
+                contrast: 0,            // lw.raster-to-gcode: Image contrast [-255 to +255]
+                gamma: 0,               // lw.raster-to-gcode: Image gamma correction [0.01 to 7.99]
+                grayscale: 'none',      // lw.raster-to-gcode: Graysale algorithm [none, average, luma, luma-601, luma-709, luma-240, desaturation, decomposition-[min|max], [red|green|blue]-chanel]
+                shadesOfGray: 256,      // lw.raster-to-gcode: Number of shades of gray [2-256]
+                invertColor: false,     // lw.raster-to-gcode
             }
         }
-        this.handleParamChange.bind(this)
+        this.timeout=0;
+        this.handleChange.bind(this)
         this.handleTrace.bind(this)
         this.handleNew.bind(this)
     }
@@ -258,15 +309,15 @@ class ImageEditor extends React.Component
         this.processImage();
     }
 
-    handleParamChange(change)
+    handleChange(key, change)
     {
-        let state= Object.assign(this.state, change)
+        let state= Object.assign(this.state[key], change)
         this.setState(state)
     }
 
     handleTrace(e){
         let [wpx, hpx] = this.currentDocument.originalPixels;
-        Potrace.loadImageFromUrl(this.image.src)
+        Potrace.loadImageFromUrl(this.canvas.toDataURL())
         Potrace.setParameter(this.state.params)
         Potrace.process(function(){
             let svg=Potrace.getSVG(1)
@@ -304,36 +355,54 @@ class ImageEditor extends React.Component
         let documents = this.props.data.documents
             .filter(d => (d.selected))
 
-        if (documents.length)
-        {
-            this.currentDocument = documents[0]
-            this.image.src=this.currentDocument.dataURL;
+        
+        clearTimeout(this.timeout)
+        this.timeout=setTimeout(function(){
+            if (documents.length) {
+                this.currentDocument = documents[0];
+                promisedImage(this.currentDocument.dataURL).then(function(image){
+                    this.canvas.width = image.width;
+                    this.canvas.height = image.height;
+                    this.canvas.getContext("2d").drawImage(image, 0, 0)
+                    canvasFilters(this.canvas, this.state.filters)
+                }.bind(this))
+            }
+        }.bind(this),200)
+        
             
-        }
+       
     }
 
     render()
     {
-        let EnumTurn=EnumInput(['minority', 'majority','black','white','right', 'left', 'random']);
+        
         return <ImageEditorModal modal={{ show: this.props.show, onHide: this.props.onHide }}
                 header="Image Editor"
+                footer={<div><Button onClick={e=>this.handleTrace(e)}>Trace</Button>
+                <Button bsStyle="success" onClick={e=>this.handleNew(e)} disabled={!this.state.svg}>New Doc</Button></div>}
             >
             <div className="trace-image">
                 <div className="showroom checker">
-                    <img ref={(i)=>{this.image=i}} src="" />
+                    <canvas ref={(i)=>{this.canvas=i}} />
                     <img ref={(i)=>{this.trace=i}} src="" />
                 </div>
-                <div className="filters">
-                    
-                </div>
                 <div className="controls">
-                    <div>Turn Policy <EnumTurn op={this.state.params} field="turnpolicy" onChange={v => this.handleParamChange({'turnpolicy':v.target.value}) } /></div>
-                    <div>Turd Size <Input Component={FormControl} type="number" onChangeValue={v => this.handleParamChange({'turdsize':v}) } value={this.state.params['turdsize']} /></div>
-                    <div>Alpha Max <Input Component={FormControl} type="number" onChangeValue={v => this.handleParamChange({'alphamax':v}) } value={this.state.params['alphamax']} /></div>
-                    <div>Opt Curve <Toggle id={"toggle_optcurve"} defaultChecked={this.state.params['optcurve'] == true} onChange={e => this.handleParamChange({'optcurve':e.target.checked })} /></div>
-                    <div>Opt Tolerance <Input Component={FormControl} type="number" onChangeValue={v => this.handleParamChange({'opttolerance':v}) } value={this.state.params['opttolerance']} /></div>
-                    <Button onClick={e=>this.handleTrace(e)}>Trace</Button>
-                    <Button bsStyle="success" onClick={e=>this.handleNew(e)} disabled={!this.state.svg}>New Doc</Button>
+                    <div>
+                        <h4>Filters</h4>
+                        {Object.entries(ImageFilterControls).map((e,i)=>{
+                            let [key, data] = e;
+                            let Field = data.input;
+                            return <div key={i}>{data.label} <Field op={this.state.filters} field={data} onChangeValue={v => this.handleChange('filters',{[key]:v}) } /></div>
+                        })}
+                    </div>
+                    <div>
+                        <h4>Trace</h4>
+                        {Object.entries(ImageTraceControls).map((e,i)=>{
+                            let [key, data] = e;
+                            let Field = data.input;
+                            return <div key={i}>{data.label} <Field op={this.state.params} field={data} onChangeValue={v => this.handleChange('params',{[key]:v}) } /></div>
+                        })}
+                    </div>
                 </div>
             </div>
             
