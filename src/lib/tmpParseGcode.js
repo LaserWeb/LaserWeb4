03cb8f208ca0f96d69e7d3634aa0ebf3823d5c0a
Owen <owen@easytarget.org>
2022-01-17 18:49:13 +0100

    remove debug logging

--- src/lib/tmpParseGcode.js
+++ src/lib/tmpParseGcode.js
@@ -4,12 +4,12 @@
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -46,7 +46,6 @@ export function parseGcode(gcode) {
                 f = parse();
                 if (isNaN(lastF))
                     lastF = f;
-                console.log('ParseGcode: F: ' + ' :' + f);
             }
             else if (gcode[i] == 'S' || gcode[i] == 's')
                 lastS = parse();
