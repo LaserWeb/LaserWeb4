568bd2ff2fbce0dd9e6f33d3c2e0868a7a257028
Owen <owen.carter@gmail.com>
2022-01-28 14:09:07 +0100

    WebPack3 config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ var path = require('path');
 var src_path = path.resolve('./src');
 var dist_path = path.resolve('./dist');
 
+// https://webpack.js.org/migrate/3/#module-loaders-is-now-module-rules
+
 module.exports = {
     context: src_path,
     entry: [
@@ -15,60 +17,116 @@ module.exports = {
     },
     node: { "fs": "empty" },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
                 loader: 'babel-loader',
                 query: {
                     presets: ['react'],
                     plugins: ['transform-es2015-destructuring', 'transform-es2015-parameters', 'transform-object-rest-spread', 'transform-es2015-modules-commonjs', 'react-hot-loader/babel']
                 }
-            }, {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            }, {
-                test: /\.png$/,
-                loader: 'url-loader?limit=100000'
-            }, {
-                test: /\.jpg$/,
-                loader: 'file-loader'
-            }, {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-            }, {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-            }, {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file-loader'
-            }, {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-            }, {
-                test: /\.json$/,
-                loader: 'json-loader'
-            }, {
-                test: /\.wasm$/,
+            }],
+        },
+        {
+            test: /\.css$/,
+            use: [{
+                loader: "style-loader"
+            },
+            {
+                loader: "css-loader"
+            }]
+        },
+        {
+            test: /\.png$/,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 100000
+                }
+            }]
+        },
+        {
+            test: /\.jpg$/,
+            use: [{
+                loader: "file-loader"
+            }]
+        },
+        {
+            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff"
+                }
+            }]
+        },
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/octet-stream"
+                }
+            }]
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+                loader: "file-loader"
+            }]
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "image/svg+xml"
+                }
+            }]
+        },
+        {
+            test: /\.wasm$/,
+            use: [{
                 loader: "file-loader",
                 options: {
                     publicPath: "dist/"
                 }
-            }, {
-                test: /\.md$/,
+            }]
+        },
+        {
+            test: /\.md$/,
+            use: [{
                 loader: "markdown-loader"
-            }, {
-                test: /\.swf$/,
-                loader: "file-loader?name=[path][name].[ext]"
-            }, {
-                test: require.resolve('snapsvg'),
-                loader: 'imports-loader?this=>window,fix=>module.exports=0'
-            },
+            }]
+        },
+        {
+            test: /\.swf$/,
+            use: [{
+                loader: "file-loader",
+                options: {
+                    name: "[path][name].[ext]"
+                }
+            }]
+        },
+        {
+            test: require.resolve('snapsvg/dist/snap.svg.js'),
+            use: 'imports-loader?this=>window,fix=>module.exports=0'
+        }],
+        loaders: [
         ]
     },
     plugins: [
         new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'}),
         new webpack.HotModuleReplacementPlugin(),
     ],
+    resolve: {
+        alias: {
+            snapsvg: 'snapsvg/dist/snap.svg.js',
+        },
+    },
     devtool: 'source-map'
 };
