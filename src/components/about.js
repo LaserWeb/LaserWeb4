57285f128f83d547a9a7641286dd8bb5ca5177c3
Owen <owen.carter@gmail.com>
2022-02-03 16:34:27 +0100

    first pass at about page link correction

--- src/components/about.js
+++ src/components/about.js
@@ -39,32 +39,48 @@ import Toggle from 'react-toggle'
                     <dt><Icon name="usb"/> Backend: {this.props.settings.comServerVersion} (API: {this.props.settings.comApiVersion}) </dt><dd></dd><p/>
                   </dl>
                   <Releases/>
-                <h3>Support Communities</h3>
+                <h3>Support</h3>
                   <dl>
-                    <dt><Icon name="users"/> <a href="https://github.com/LaserWeb">LaserWeb Github Organisation</a></dt>
-                    <dd><small>- developer community</small></dd><p/>
-                    <dt><Icon name="users"/> <a href="https://forum.makerforums.info/c/laserweb-cncweb/">LaserWeb Community on MakerForums</a></dt>
-                    <dd><small>- support Community for this software itself</small></dd><p/>
+                    <dt><Icon name="hashtag"/> <a href="https://forum.makerforums.info/c/laserweb-cncweb/">LaserWeb Community on MakerForums</a></dt>
+                    <dd><small>- support Community for this software</small></dd>
+                    <dd><small>- this is the place to visit for support questions and answers</small></dd><p/>
+                  </dl>
+                <h4>Communities</h4>
+                  <dl>
+                    <dt><Icon name="anchor"/> <a href="https://forum.makerforums.info/">MakerForums Communities</a></dt>
+                    <dd><small>- support communities for Makers</small></dd><p/>
                     <dt><Icon name="users"/> <a href="https://forum.makerforums.info/c/k40/">K40 Laser MakerForum Community</a></dt>
                     <dd><small>- support community for popular K40 CO2 Lasers</small></dd><p/>
-                    <dt><Icon name="users"/> <a href="https://forum.makerforums.info/">All MakerForums Communities</a></dt>
-                    <dd><small>- support communities for Makers</small></dd><p/>
+                    <dt><Icon name="sitemap"/> <a href="https://forum.makerforums.info/c/halfnormal-toolbox/">HalfNormal's Toolbox</a></dt>
+                    <dd><small>- curated links to the best online tools and ideas</small></dd><p/>
+                    <dt><Icon name="github"/> <a href="https://github.com/LaserWeb">LaserWeb Github Organisation</a></dt>
+                    <dd><small>- developer community</small></dd><p/>
                   </dl>
-                <h3>Developers</h3>
+                <h4>Creators</h4>
                   <dl>
-                    <dt><Icon name="user"/> <a href="https://plus.google.com/101442607030198502072">Todd Fleming</a> <a target="_blank" href="https://paypal.me/tbfleming"><Icon name="paypal"/></a></dt>
+                    <dt><Icon name="user"/> <a href="https://plus.google.com/101442607030198502072">Todd Fleming</a>&nbsp;<a target="_blank" href="https://paypal.me/tbfleming"><Icon name="paypal"/></a></dt>
                     <dd><small>- G-Code Generation, 3D viewer, Simulator</small></dd><p/>
-                    <dt><Icon name="user"/> <a href="https://plus.google.com/+S%C3%A9bastienMischler-Skarab">Sebastien Mischler</a> <a target="_blank" href="https://paypal.me/skarab"><Icon name="paypal"/></a></dt>
+                    <dt><Icon name="user"/> <a href="https://plus.google.com/+S%C3%A9bastienMischler-Skarab">Sebastien Mischler</a>&nbsp;<a target="_blank" href="https://paypal.me/skarab"><Icon name="paypal"/></a></dt>
                     <dd><small>- Backend + Dev environment</small></dd><p/>
-                    <dt><Icon name="user"/> <a href="https://plus.google.com/113562432484049167641">Jorge Robles</a> <a target="_blank" href="https://paypal.me/JorgeDredd"><Icon name="paypal"/></a></dt>
-                    <dd><small>- Settings modules, UI tweaks, Workspace Export</small></dd><p/>
-                    <dt><Icon name="user"/> <a href="https://plus.google.com/+ClaudioPrezzi">Claudio Prezzi</a> <a target="_blank" href="https://paypal.me/cprezzi"><Icon name="paypal"/></a></dt>
-                    <dd><small>- Communications</small></dd><p/>
-                    <dt><Icon name="user"/> <a href="https://plus.google.com/+PetervanderWalt">Peter van der Walt</a> <a target="_blank" href="https://paypal.me/openhardwarecoza"><Icon name="paypal"/></a></dt>
+                    <dt><Icon name="user"/> <a href="https://plus.google.com/+PetervanderWalt">Peter van der Walt</a>&nbsp;<a target="_blank" href="https://paypal.me/openhardwarecoza"><Icon name="paypal"/></a></dt>
                     <dd><small>- Support + User Interface</small></dd><p/>
                     <dt><Icon name="user"/> <a href="https://plus.google.com/+ArielYahni">Ariel Yahni</a></dt>
                     <dd><small>- Beta testing</small></dd><p/>
                   </dl>
+                <h4>Recent Developers</h4>
+                  <dl>
+                    <dt><Icon name="user"/> <a href="https://plus.google.com/+ClaudioPrezzi">Claudio Prezzi</a>&nbsp;<a target="_blank" href="https://paypal.me/cprezzi"><Icon name="paypal"/></a></dt>
+                    <dd><small>- Communications server, documentation</small></dd>
+                    <dd><small>- Maintenance of both the software and the community for many years</small></dd><p/>
+                    <dt><Icon name="user"/> <a href="https://plus.google.com/113562432484049167641">Jorge Robles</a>&nbsp;<a target="_blank" href="https://paypal.me/JorgeDredd"><Icon name="paypal"/></a></dt>
+                    <dd><small>- Settings modules, UI tweaks, Workspace Export</small></dd><p/>
+                    <dt><Icon name="user"/> <a href="https://www.easytarget.org/">Owen Carter</a></dt>
+                    <dd><small>- v4.1 Frontend improvements, build and packaging maintenance</small></dd>
+                    <dd><small>- Currently looking for interesting work; find me on <a target="_blank" href="https://www.linkedin.com/in/easytarget/">LinkedIn&nbsp;<Icon name="linkedin-square"/></a></small></dd><p/>
+                  </dl>
+
+                <div>If you would like to assist with the future development of Laserweb please find us on <a target="_blank" href="https://github.com/LaserWeb"><Icon name="github"/>&nbsp;GitHub</a>; we are especially looking for contributors with strong Javascript, React and Redux skills to help with core architecture upgrades</div>
+                <hr/>
                 <div><i>LaserWeb and CNCWeb is <kbd>free software</kbd>. The team of developers have spent countless hours coding, testing and supporting this application. If you enjoy using this application, consider donating a coffee or a beer towards the developers to show your appreciation, by clicking the <Icon name="paypal"/> icon next to the developers you want to support</i></div>
 
                 <hr/>
